02f422f6e7260428df1f2179939e1224aec922a9
Marta Piechówka <piechowkamarta@gmail.com>
2023-04-01 15:43:02 +0200

    refactor

--- backend/server.py
+++ backend/server.py
@@ -58,7 +58,7 @@ async def move(moveRequest: MoveRequest):
 
 
 @app.get("/getchat")
-async def get_chat(get_chat_request: GetChatRequest) -> GetChatResponse:
+async def get_chat(get_chat_request: GetChatRequest) -> GetChatResponse | JSONResponse:
     chat = chats[get_chat_request.chat_id]
     if not (chat["is_private"] and get_chat_request.user_id not in chat["users_ids"].keys()):
         return GetChatResponse(msg=chat["messages"])
@@ -76,7 +76,7 @@ async def join_chat(join_chat_request: JoinChatRequest):
 
 
 @app.post("/userlogin")
-async def create_chat(create_chat_request: CreateChatRequest) -> CreateChatResponse:
+async def create_chat(create_chat_request: CreateChatRequest) -> CreateChatResponse | JSONResponse:
     if users[create_chat_request.user_id2]["status"] != "not disturb":
         new_chat_id = uuid.uuid4()
         chats[new_chat_id] = {"users_ids": {create_chat_request.user_id1: True, create_chat_request.user_id2: True},
@@ -101,22 +101,3 @@ def leave_chat(user_id, chat_id):
     if chat["active_users_count"] == 0:
         # delete chat and archive conversation and remove it from current chats
         pass
-
-
-@app.route('/get_map_state', methods=['GET'])
-def get_map_state(user_id: int):
-    return get_map_state(user_id)
-
-
-@app.route('/write_msg', methods=['PUT'])
-def write_msg(user_id: int, chat_id: int, msg: str):
-    return write_msg(user_id, chat_id, msg)
-
-
-@app.route('/leave_chat', methods=['PUT'])
-def leave_chat(user_id: int, chat_id: str):
-    return leave_chat(user_id, chat_id)
-
-
-if __name__ == "__main__":
-    app.run()
