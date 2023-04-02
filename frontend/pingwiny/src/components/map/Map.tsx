f6db534075299dd2a9274e373474cbb5a8ed878c
Magdalena Skarbinska <mskarbinska@protonmail.com>
2023-04-02 10:20:37 +0200

    add lets talk cloud and nicknames

--- frontend/pingwiny/src/components/map/Map.tsx
+++ frontend/pingwiny/src/components/map/Map.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, useEffect, useState } from 'react';
 import { Stage} from '@pixi/react';
 import Desk from '../../types/Desk';
 import { Graphics } from '@inlet/react-pixi';
 import MyPenguin from '../penguins/MyPenguin';
 import User from '../../types/User';
 import Penguin from '../penguins/Penguin';
+import PenguinsContainer from '../penguins/PenguinsContainer';
 
 interface Props{
     desks: Desk[],
-    user: User
+    user: User,
+    setUser: Dispatch<User>
   }
 
-const Map = ({desks, user}:Props) => {
+const Map = ({desks, user, setUser}:Props) => {
     const penguinUsers: User[] = [
         {x: 100, y: 100, nickname: "Pinguin1", id:"4"},
         {x: 600, y: 400, nickname: "Pinguin2", id:"7"},
     ]
 
     return (
-    <Stage width={1100} height={750} options={{ backgroundColor: "e0ebeb", antialias: true }}>
+    <Stage width={window.innerWidth*0.7} height={window.innerHeight} options={{ backgroundColor: "e0ebeb", antialias: true }}>
         {desks.map((desk, index)=>
         <Graphics
         key={index}
@@ -30,13 +32,7 @@ const Map = ({desks, user}:Props) => {
         }}
         />
         )}
-        {penguinUsers.map((penguinUser, index)=>
-            <Penguin
-            key={index}
-            user={penguinUser}
-        />
-        )}
-        <MyPenguin user={user}/>
+        <PenguinsContainer user={user} penguins={penguinUsers} setUser={setUser}/>
     </Stage>
     )
 }
